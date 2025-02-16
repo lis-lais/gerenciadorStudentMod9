68cccbf215dd9cfbb7e9bee8be7d07ca551eae3e
Laís Lima <143414398+lis-lais@users.noreply.github.com>
2025-02-16 11:14:59 -0300

    Update studentRepository.js
    Corrigido o erro em updateStudentById.

--- src/repositories/studentRepository.js
+++ src/repositories/studentRepository.js
@@ -3,7 +3,7 @@ const Student = require('../models/student');
 const createStudent = async (studentData) => await Student.create(studentData);
 const findAllStudents = async () => await Student.find ();
 const findStudentById = async (id) => await Student.findById(id);
-const updateStudentById = async (id, studentData) => await Student.findByIdAndUpdate(id, studentData, { new: true });
+const updateStudentById = async (id, data) => await Student.findByIdAndUpdate(id, data, { new: true });
 const deleteStudentById = async (id) => await Student.findByIdAndDelete(id);
 const searchStudent = async (filters) => await Student.find(filters);
 
